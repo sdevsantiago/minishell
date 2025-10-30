64610183aa20766715c983f6fcf864f88f36ed7b
Sergio <sergio.desantiago@hotmail.es>
2025-10-30 12:07:21 +0100

    update: updated parameters for pwd builtin

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 22:09:51 by sede-san          #+#    #+#             */
-/*   Updated: 2025/10/30 01:21:55 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/10/30 12:06:39 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ extern u_int8_t	builtin_exit(t_command cmd, t_minishell *msh);
 
 // pwd.c
 
-extern u_int8_t	builtin_pwd(void);
+extern u_int8_t	builtin_pwd(t_command cmd, t_minishell *msh);
 
 #endif /* BUILTINS_H */
